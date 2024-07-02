572eb7923ba80e07dc09544278988d1008ce0346
deviant101 <115878714+deviant101@users.noreply.github.com>
2024-07-02 13:52:47 +0500

    vnet-peering added

--- main.tf
+++ main.tf
@@ -35,3 +35,29 @@ resource "azurerm_subnet" "B-subnet" {
   virtual_network_name = azurerm_virtual_network.B.name
   address_prefixes     = ["10.15.0.0/24"]
 }
+
+##################### Peering from Vnet-A to Vnet-B #####################
+resource "azurerm_virtual_network_peering" "A-to-B" {
+  name                      = "A2B"
+  resource_group_name       = azurerm_resource_group.peering.name
+  virtual_network_name      = azurerm_virtual_network.A.name
+  remote_virtual_network_id = azurerm_virtual_network.B.id
+  # allow_virtual_network_access = true
+  # allow_forwarded_traffic = true
+  # allow_gateway_transit = true
+  # use_remote_gateways = false
+
+}
+
+##################### Peering from Vnet-B to Vnet-A #####################
+resource "azurerm_virtual_network_peering" "B-to-A" {
+  name                      = "B2A"
+  resource_group_name       = azurerm_resource_group.peering.name
+  virtual_network_name      = azurerm_virtual_network.B.name
+  remote_virtual_network_id = azurerm_virtual_network.A.id
+  # allow_virtual_network_access = true
+  # allow_forwarded_traffic = true
+  # allow_gateway_transit = true
+  # use_remote_gateways = false
+
+}
